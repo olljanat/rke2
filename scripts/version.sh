2db779d9a42d32f488817d99de38642c7cc9f706
Olli Janatuinen <olli.janatuinen@gmail.com>
2022-09-27 19:23:48 +0000

    Hardcode version to allow simpler prototyping

--- scripts/version.sh
+++ scripts/version.sh
@@ -31,32 +31,13 @@ REVISION=$(git rev-parse HEAD)$(if ! git diff --no-ext-diff --quiet --exit-code;
 PLATFORM=${GOOS}-${GOARCH}
 RELEASE=${PROG}.${PLATFORM}
 # hardcode versions unless set specifically
-KUBERNETES_VERSION=${KUBERNETES_VERSION:-v1.23.12}
-KUBERNETES_IMAGE_TAG=${KUBERNETES_IMAGE_TAG:-v1.23.12-rke2r1-build20220921}
+KUBERNETES_VERSION=v1.23.12
+KUBERNETES_IMAGE_TAG=v1.23.12-rke2r1-build20220921
 ETCD_VERSION=${ETCD_VERSION:-v3.5.4-k3s1}
 PAUSE_VERSION=${PAUSE_VERSION:-3.6}
 CCM_VERSION=${CCM_VERSION:-v0.0.3-build20211118}
 
-if [ -d .git ]; then
-    if [ -z "$GIT_TAG" ]; then
-        GIT_TAG=$(git tag -l --contains HEAD | head -n 1)
-    fi
-    if [ -n "$(git status --porcelain --untracked-files=no)" ]; then
-        DIRTY="-dirty"
-        TREE_STATE=dirty
-    fi
-
-    COMMIT=$(git log -n3 --pretty=format:"%H %ae" | grep -v ' drone@localhost$' | cut -f1 -d\  | head -1)
-    if [ -z "${COMMIT}" ]; then
-        COMMIT=$(git rev-parse HEAD || true)
-    fi
-fi
-
-if [[ -n "$GIT_TAG" ]]; then
-    VERSION=$GIT_TAG
-else
-    VERSION="${KUBERNETES_VERSION}-dev+${COMMIT:0:8}$DIRTY"
-fi
+VERSION="${KUBERNETES_VERSION}+rke2r1"
 
 if [[ "${VERSION}" =~ ^v([0-9]+)\.([0-9]+)(\.[0-9]+)?([-+].*)?$ ]]; then
     VERSION_MAJOR=${BASH_REMATCH[1]}
